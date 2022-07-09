dbbca49e2eb2dea92d72474287b992ecafcaf77b
falihnurandino <andinofalih1@gmail.com>
2022-07-09 16:30:22 +0700

    initial commit

---
+++ src/utils/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Falih Nur Andino',
+            tag: 'falihnurandino',
+            imageUrl: '/images/default.jpg'
+        },
+        {
+            id: 2,
+            name: 'Gabriel Rolas',
+            tag: 'gabrielrolas',
+            imageUrl: '/images/default.jpg'
+        },
+        {
+            id: 3,
+            name: 'Fajar Haryanto',
+            tag: 'fajarharyanto',
+            imageUrl: '/images/default.jpg'
+        },
+    ]
+}
+
+export {getData};
