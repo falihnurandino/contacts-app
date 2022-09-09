0b166828d706154a8cc14e52b3bfcac94d304c39
falihnurandino <andinofalih1@gmail.com>
2022-09-09 20:13:07 +0700

    add PropTypes

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import PropType from 'prop-types';
 
-function DeleteButton({id, onDelete}) {
-    return <button className='contact-item__delete' onClick={() => onDelete(id)}>X</button>
+function DeleteButton({ id, onDelete }) {
+  return (
+    <button className="contact-item__delete" onClick={() => onDelete(id)}>
+      X
+    </button>
+  );
 }
+
+DeleteButton.propType = {
+  id: PropType.number.isRequired,
+  onDelete: PropType.func.isRequired,
+};
 
 export default DeleteButton;
