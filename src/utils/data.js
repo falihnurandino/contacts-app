854bdd1b13e82fbf0ca1ccb260c548e6c73c4abf
falihnurandino <andinofalih1@gmail.com>
2022-09-11 06:43:39 +0700

    add data to data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,24 +1,30 @@
 const getData = () => {
-    return [
-        {
-            id: 1,
-            name: 'Falih Nur Andino',
-            tag: 'falihnurandino',
-            imageUrl: '/images/default.jpg'
-        },
-        {
-            id: 2,
-            name: 'Gabriel Rolas',
-            tag: 'gabrielrolas',
-            imageUrl: '/images/default.jpg'
-        },
-        {
-            id: 3,
-            name: 'Fajar Haryanto',
-            tag: 'fajarharyanto',
-            imageUrl: '/images/default.jpg'
-        },
-    ]
-}
+  return [
+    {
+      id: 1,
+      name: 'Falih Nur Andino',
+      tag: 'falihnurandino',
+      imageUrl: '/images/default.jpg',
+    },
+    {
+      id: 2,
+      name: 'Gabriel Rolas',
+      tag: 'gabrielrolas',
+      imageUrl: '/images/default.jpg',
+    },
+    {
+      id: 3,
+      name: 'Fajar Haryanto',
+      tag: 'fajarharyanto',
+      imageUrl: '/images/default.jpg',
+    },
+    {
+      id: 4,
+      name: 'Aprillian Abu',
+      tag: 'aprillianabu',
+      imageUrl: '/images/default.jpg',
+    },
+  ];
+};
 
-export {getData};
+export { getData };
