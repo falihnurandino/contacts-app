89d1b9b7f1185642aa28cc5a2ef2f1b6528e2d58
falihnurandino <andinofalih1@gmail.com>
2022-09-11 07:25:50 +0700

    add property validation to ContactInput

--- src/components/ContactInput.js
+++ src/components/ContactInput.js
@@ -1,51 +1,70 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ContactInput extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            name: '',
-            tag: '',
-        }
-
-        this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
-        this.onTagChangeEventHandler = this.onTagChangeEventHandler.bind(this);
-        this.onSubmitChangeEventHandler = this.onSubmitChangeEventHandler.bind(this);
-    }
-
-    onNameChangeEventHandler(event) {
-        this.setState((prevState) => {
-            return {
-                ...prevState,
-                name: event.target.value,
-            }
-        });
-    }
-
-    onTagChangeEventHandler(event) {
-        this.setState((prevState) => {
-            return {
-                ...prevState,
-                tag: event.target.value,
-            }
-        });
-    }
-
-     onSubmitChangeEventHandler(event) {
-        event.preventDefault();
-        this.props.addContact(this.state);
-    }
-
-    render() {
-        return (
-            <form className='contact-input' onSubmit={this.onSubmitChangeEventHandler}>
-                <input type='text' placeholder='Nama' value={this.state.name} onChange={this.onNameChangeEventHandler}/>
-                <input type='text' placeholder='Tag' value={this.state.tag} onChange={this.onTagChangeEventHandler}/>
-                <button type='submit'>Tambah</button>
-            </form>
-        )
+    this.state = {
+      name: '',
+      tag: '',
     };
+
+    this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+    this.onTagChangeEventHandler = this.onTagChangeEventHandler.bind(this);
+    this.onSubmitChangeEventHandler =
+      this.onSubmitChangeEventHandler.bind(this);
+  }
+
+  onNameChangeEventHandler(event) {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        name: event.target.value,
+      };
+    });
+  }
+
+  onTagChangeEventHandler(event) {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        tag: event.target.value,
+      };
+    });
+  }
+
+  onSubmitChangeEventHandler(event) {
+    event.preventDefault();
+    this.props.addContact(this.state);
+  }
+
+  render() {
+    return (
+      <form
+        className="contact-input"
+        onSubmit={this.onSubmitChangeEventHandler}
+      >
+        <input
+          type="text"
+          placeholder="Nama"
+          value={this.state.name}
+          onChange={this.onNameChangeEventHandler}
+        />
+        <input
+          type="text"
+          placeholder="Tag"
+          value={this.state.tag}
+          onChange={this.onTagChangeEventHandler}
+        />
+        <button type="submit">Tambah</button>
+      </form>
+    );
+  }
 }
+
+ContactInput.propTypes = {
+  addContact: PropTypes.func.isRequired,
+};
 
 export default ContactInput;
