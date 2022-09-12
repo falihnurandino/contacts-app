5dd715c6dd59af50074248baf934da1527b8f199
falihnurandino <andinofalih1@gmail.com>
2022-09-12 10:31:58 +0700

    add searchbar to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContactList from '../components/ContactList';
+import SearchBar from '../components/SearchBar';
 import { deleteContact, getContacts } from '../utils/data';
 
 class HomePage extends React.Component {
@@ -8,9 +9,11 @@ class HomePage extends React.Component {
 
     this.state = {
       contacts: getContacts(),
+      keyword: '',
     };
 
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
   }
 
   onDeleteHandler(id) {
@@ -23,14 +26,29 @@ class HomePage extends React.Component {
     });
   }
 
+  onKeywordChangeHandler(keyword) {
+    this.setState(() => {
+      return {
+        keyword,
+      };
+    });
+  }
+
   render() {
+    const contacts = this.state.contacts.filter((contact) => {
+      return contact.name
+        .toLowerCase()
+        .includes(this.state.keyword.toLowerCase());
+    });
+
     return (
       <section>
-        <h2>Daftar Kontak</h2>
-        <ContactList
-          contacts={this.state.contacts}
-          onDelete={this.onDeleteHandler}
+        <SearchBar
+          keyword={this.state.keyword}
+          keywordChange={this.onKeywordChangeHandler}
         />
+        <h2>Daftar Kontak</h2>
+        <ContactList contacts={contacts} onDelete={this.onDeleteHandler} />
       </section>
     );
   }
